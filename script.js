4c274137a616b03477eba977a18ba240f09fce91
N-eeraj <neeraj7rajeena@gmail.com>
2023-03-21 11:45:53 +0530

    Added reveal animation

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const contentList = document.querySelectorAll('.content')
 section.style.height = `${100 * contentList.length}dvh`
 contentList.forEach(element => element.style.height = `${100 / contentList.length}%`)
 
-contentList[0].classList.add('active')
+let active = 0
+
+contentList[active].classList.add('active')
+
 
 window.addEventListener('scroll', e => {
+    const newActive = Math.round(window.scrollY / window.innerHeight)
+    if (newActive === active) return
+    active = newActive
     document.querySelector('.active').classList.remove('active')
-    contentList[Math.round(window.scrollY / window.innerHeight)].classList.add('active')
+    contentList[active].classList.add('active')
 })
